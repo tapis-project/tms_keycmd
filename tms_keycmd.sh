26e8cc2b5395e082a9cd6b5a7759010d3304c4d4
Steve Black <scblack@tacc.utexas.edu>
2024-05-26 11:58:53 -0500

    Add wrapper script for launching rust program.

---
+++ tms_keycmd.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Basic wrapper script for running TMS KeyCmd program from
+#   an arbitrary location.
+#
+# Determine current directory
+set -xv
+RUN_DIR="$(pwd -P)"
+
+# Determine absolute path to location from which we are running.
+# Note that output is sent to /dev/null in the unlikely case that the cd command
+#   generates extraneous output that would interfere with the surrounding $()
+# The -P option on pwd is for better handling of symlinks.
+# The "--"  on cd is in case the directory begins with "-". The "--"
+#   tells the shell that there are no more options.
+PRG_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+# Change to the directory where script is installed.
+cd $PRG_PATH
+
+# Run the TMS KeyCmd program with all arguments passed on the command line.
+# Note that "$@" is used so that original order and grouping is perserved.
+#   For example, input arguments might look like this: "a b 'c d'"
+./tms_keycmd "$@"
+
+# Return to original directory
+cd $RUN_DIR
