8df8f4800a4466caa4b2d3b6586eacf818e7ff72
Steve Black <scblack@tacc.utexas.edu>
2025-02-11 16:17:04 -0600

    Update test script get_pubkey.

--- test_flow/get_pubkey.sh
+++ test_flow/get_pubkey.sh
@@ -5,7 +5,4 @@
 REQ_JSON=$1
 set -xv
 curl -k -X POST -H "content-type: application/json" \
-       	-H "X-TMS-TENANT: $TMS_TENANT" \
-       	-H "X-TMS-CLIENT-ID: $TMS_CLIENT_ID" \
-       	-H "X-TMS-CLIENT-SECRET: $TMS_CLIENT_KEY" \
 	$TMS_URL/v1/tms/pubkeys/creds/retrieve -d @$1
